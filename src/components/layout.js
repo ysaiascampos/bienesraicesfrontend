7d991dbbd863cfdb308eff70f18764479844c9dd
ysaiascampos <ysaiascampos@gmail.com>
2020-12-28 22:51:00 -0300

    continuacion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,63 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/react';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+    return ( 
+        <>
+            <Global
+                styles={css`
+                    body {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
+                    body {
+                        font-size: 1.6rem;
+                        line-height: 2;
+                        font-family: 'Lato', sans-serif;
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        text-align: center;
+                        font-family: 'Lato', sans-serif;
+                        font-weight: 300;
+                    }
+                    h3 {
+                        font-family: 'Roboto', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .contenedor {
+                        max-width: 120rem;
+                        margin: 0 auto;
+                        width: 95%;
+                    }
+                    img {
+                        max-width: 100%;
+                    }
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+                `}
+            />
+            <Helmet>
+                <title>Bienes Raices Gatsby</title>
+                <meta name="descripcion" content="Sitio web de bienes raices en Gatsby" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+            </Helmet>
+            <header>menu</header>
+            { children }
+            <footer></footer>
+        </>
+     );
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+ 
+export default Layout;
